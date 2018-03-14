d4d42c22d9c916d168698b2f115af886d2c06503
andreydro <andreydro14@gmail.com>
2018-03-14 19:38:32 +0300

    Trying-eslint

--- isPalindrom.js
+++ isPalindrom.js
@@ -1,21 +1,21 @@
 // right code 
 
 function isPalindrom(str) {
-  if(str[0] == str[str.length-1]) {
-    return true;
-    } else if (str === ""){
-    return true;
+    if (str[0] == str[str.length - 1]) {
+        return true;
+    } else if (str === "") {
+        return true;
     } else {
-    return false;
+        return false;
     }
 }
-
 isPalindrom("ASasdasd");
 
 
 // the same task in less then 50 symbols (in progress...)
 
 function isPalindrom(s) {
- var r = s.split("").reverse().join("");
- return r == s || s == "" ? true : false;
+    var r = s.split("").reverse().join("");
+    return r == s || s == "" ? 1 : 0;
 }
+
