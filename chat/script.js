0fb6316f26c3ba6d457b8d488596714aabf24566
andreydro <andreydro14@gmail.com>
2018-06-30 23:07:34 +0300

    Update chat

--- chat/script.js
+++ chat/script.js
@@ -1,3 +1,4 @@
+'use strict'
 var input = document.querySelector('#le_inpute');// {message, name, date}
 var room = '/group2.json';
 var url = 'https://rest-examples-6cb6b.firebaseio.com';
@@ -9,7 +10,7 @@ var output = document.querySelector('#output');
 button.addEventListener('click', function() {
   postMessage(input.value, 'group2');
 });
-
+  
 function postMessage(text, name) {
   fetch(url + room, {
     method: 'POST',
@@ -37,29 +38,81 @@ function listMessages(list) {
   output.innerHTML = message;
 }
 
-// https://xhr.spec.whatwg.org/
-//console.clear();
-
 var xhttp = new XMLHttpRequest();
 var targetUrl = 'https://rest-examples-6cb6b.firebaseio.com/group2.json';
-var oldResponse;
+var oldResponce = [];
+var messages = [];
+var newArray = [];
 
 xhttp.onreadystatechange = function(e) {
-  //console.log('Arguments', arguments);
-  if (this.readyState == 4 && this.status == 200) {
-    //console.log('done:', this.responseText);
-    console.log(this.responseText);
-  }else if (this.status){
-    //console.log('Progress:', this.status, this.responseText)
-    output.innerHTML = this.responseText.match(/"data":\{.*\}/)[0];
-    oldResponse = this.responseText.match(/"data":\{.*\}/)[0];
-  }else{
+  if (this.readyState == 3 && this.status == 200) {
+   // console.log(this.responseText);
+        var outputTmp = this.responseText.match(/"data":\{.*\}/);
+    outputTmp = outputTmp + "";
+    outputTmp = outputTmp.substring(7, outputTmp.length - 1);
+    outputTmp = JSON.parse(outputTmp);
+    for(var key in outputTmp) {
+      messages.push(outputTmp[key]);
+    }
+    
+    messages.forEach(function(item){
+      for(var key in item) {
+        output.innerHTML += ` Message: ${item["message"]}, user: ${item["name"]}, date: ${item["date"]} `
+      }
+    });
+  } else if (this.status) {
+  //  console.log(this.responseText);
+//     var outputTmp = this.responseText.match(/"data":\{.*\}/);
+//     outputTmp = outputTmp + "";
+//     outputTmp = outputTmp.substring(7, outputTmp.length - 1);
+//     outputTmp = JSON.parse(outputTmp);
+//     for(var key in outputTmp) {
+//       messages.push(outputTmp[key]);
+//     }
+    
+//     messages.forEach(function(item){
+//       for(var key in item) {
+//         output.innerHTML += ` Message: ${item["message"]}, user: ${item["name"]}, date: ${item["date"]} `
+//       }
+//     });
+   
+    //outputTmp = outputTmp.replace(/-.{21}/g, "");
+    //outputTmp = outputTmp.replace(/\"/g, "");
+    //outputTmp = outputTmp.split(",\\");
+    //outputTmp = outputTmp.match(/{\"date\":.*}/);
+   // console.log(outputTmp[0]);
+//     console.log(newArray);
+//     console.log(typeof outputTmp);
+//     outputTmp = outputTmp.substring(60, 131);
+//     outputTmp = JSON.parse(outputTmp);
+//     output.innerHTML =`Message: ${outputTmp["message"]}, user: ${outputTmp["name"]}, date: ${outputTmp["date"]}`
+//     console.log(typeof outputTmp);
+//     for (var key in outputTmp) {
+//       if (outputTmp.hasOwnProperty(key)) {
+//         console.log(key);
+//       }
+//     }
+//     messages = [];
+//     outputTmp = outputTmp.split('-');
+//     outputTmp.forEach(function(item) {
+//       var m = item.match(/\{.*\}/);
+//       if(m) {
+//         messages.push(m+'');
+//       }
+//     });
+//     messages = messages.filter(function(item) {return oldResponce.indexOf(item) === -1});
+//     messages[messages.length-1].replace(/"}}/,"");
+//     messages = messages.map(function(i){
+//       return JSON.parse(i);
+//     });
+//     console.log(messages);
+   // console.log(outputTmp);
+  } else {
     //console.log('onreadystatechange', arguments);
   }
 };
 
 xhttp.onloadend = function() {
-  //console.log('{LOADEND}');
 }
 
 xhttp.open("GET", targetUrl, true);
@@ -68,4 +121,4 @@ xhttp.setRequestHeader('Accept', 'text/event-stream')
 xhttp.send();
 
 //console.log(xhttp);
-console.log(xhttp.responseText);
+//console.log(xhttp.responseText);
